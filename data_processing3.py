f8972b50ac9e9f3b3ad51a4f2f98ffe2916be6bd
helloman1802 <malachibazar@gmail.com>
2018-05-09 12:57:42 -0500

    Added data_processing3. Used to format data for linear_regression.py

--- data_processing3.py
+++ data_processing3.py
@@ -2,23 +2,24 @@ import pandas as pd
 import numpy as np
 
 def data_extraction(csv):
+    days = 15
     df = pd.read_csv(csv)
-    df = df[['Open', 'Volume', 'Adj Close', 'High', 'Low']]
-    df = df.head(30)
+    df = df[['Open', 'Volume', 'Close', 'High', 'Low']]
+    df = df.head(days)
     
     
     
     
     df['Volume'] = df['Volume'].astype(int)
 
-    x = df['Low']
-    y = df['Adj Close']
+    x = df[['Low', 'High', 'Open', 'Volume']]
+    y = df['Close']
     x_np = x.as_matrix()
     y_np = y.as_matrix()
-    x_train = x_np[:20]
-    y_train = y_np[:20]
-    x_test = x_np[-20:]
-    y_test = y_np[-20:]
+    x_train = x_np[:days-1]
+    y_train = y_np[:days-1]
+    x_test = x_np[-days-1:]
+    y_test = y_np[-days-1:]
     return x_train, y_train, x_test, y_test
     
 data_extraction('GOOG.csv')
--- linear_regression.py
+++ linear_regression.py
@@ -9,31 +9,46 @@ import numpy as np
 
 lr = linear_model.LinearRegression()
 
-x_train, y_train, x_test, y_test = data_extraction('GOOG.csv')
+x_train, y_train, x_test, y_test = data_extraction('TQQQ.csv')
 
 
-y_train = np.reshape(y_train, (-1,1))
+
 y_test = np.reshape(y_test, (-1,1))
-x_train = np.reshape(x_train, (-1,1))
-x_test = np.reshape(x_test, (-1,1))
+#x_train = np.reshape(x_train, (-1,1))
+#x_test = np.reshape(x_test, (-1,1))
 
 lr.fit(x_train, y_train)
-
+print(np.shape(x_test))
 prediction = lr.predict(x_test)
-
+print(np.shape(prediction))
 df = pd.DataFrame(prediction)
 df2 = pd.DataFrame(y_test)
 frames = [df, df2]
 df = pd.concat(frames, axis=1)
 df.columns = ['Prediction', 'Actual']
 df.to_csv('predition_comparison.csv')
-print(df)
-
+#print(df)
+varience_score = 'Variance Score: {}'.format(r2_score(y_test, prediction))
 print('Mean square error: {}'.format(mean_squared_error(y_test, prediction)))
 print('Variance Score: {}'.format(r2_score(y_test, prediction)))
 
+
+"""
+fig = plt.figure()
+ax = fig.add_subplot(111)
+fig.subplots_adjust(top=0.85)
+ax.set_title('Google Stock Prediction', fontsize=14, fontweight='bold')
+ax.set_xlabel('Daily Adjusted Close Price', fontweight='bold')
+ax.set_ylabel('Daily Low Price', fontweight='bold')
+ax.text(0.95, 0.01, varience_score, verticalalignment='bottom', horizontalalignment='right',
+        transform=ax.transAxes,
+        color='green', fontsize=15)
+
+
+
 plt.scatter(x_test, y_test, color='black')
 plt.plot(x_test, prediction, color='blue', linewidth=3)
-plt.xticks(())
-plt.yticks(())
+plt.xticks(np.arange(min(x_test), max(x_test)+1, 1.0), rotation=-45)
+plt.yticks(np.arange(min(y_test), max(y_test)+1, 1.0))
 plt.show()
+"""
